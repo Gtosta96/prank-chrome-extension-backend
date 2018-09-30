08a097a26154525448cb86dad7ef9e3c2e71083b
Gtosta96 <gabrieltosta3@gmail.com>
2018-09-30 01:08:39 -0300

    extension is almost working

--- src/modules/pranks/api/model.js
+++ src/modules/pranks/api/model.js
@@ -3,14 +3,15 @@ const mongoose = require('mongoose');
 const DefaultSchema = require('../../shared/schemas/DefaultSchema');
 
 const defaultHTML = '<h1>THIS PAGE HAS BEEN BLOCKED BY THE ADMINISTRATOR</h1><p>Contact the support.</p>';
-const fallbackURLS = 'https://www.facebook.com/;https://www.facebook.com.br/';
+const defaultCSS = '';
+const defaultURLS = 'https://www.facebook.com/;https://www.facebook.com.br/';
 
 const schema = new DefaultSchema({
-  urls: { type: String, default: fallbackURLS },
   html: { type: String, default: defaultHTML },
-  css: String,
+  css: { type: String, default: defaultCSS },
+  urls: { type: String, default: defaultURLS },
 
-  disabled: { type: Boolean, default: true },
+  enabled: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('Prank', schema, 'pranks');
--- src/modules/pranks/ws/update.js
+++ src/modules/pranks/ws/update.js
@@ -1,10 +1,10 @@
 const PranksDAO = require('../api/dao');
 
 const update = (io, socket) => {
-  socket.on('update', (id, prank) => {
-    console.log('::PRANKS - UPDATE::', id, prank);
+  socket.on('update', (prank) => {
+    console.log('::PRANKS - UPDATE::', prank);
 
-    PranksDAO.update({ id, prank })
+    PranksDAO.update({ id: prank.id, prank })
       .then((response) => io.emit('update', response))
       .catch((error) => console.log(error));
   });
