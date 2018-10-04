0454c4c0165d5ffa7ad52f8f75fe405d4873293c
Gtosta96 <gabrieltosta3@gmail.com>
2018-10-04 19:53:39 -0300

    changed urls to be array of strings

--- src/modules/pranks/api/model.js
+++ src/modules/pranks/api/model.js
@@ -4,14 +4,15 @@ const DefaultSchema = require('../../shared/schemas/DefaultSchema');
 
 const defaultHTML = '<h1>THIS PAGE HAS BEEN BLOCKED BY THE ADMINISTRATOR</h1><p>Contact the support.</p>';
 const defaultCSS = '';
-const defaultURLS = 'https://www.facebook.com/;https://www.facebook.com.br/';
+const defaultURLS = ['facebook', 'google'];
+const defaultEnabled = false;
 
 const schema = new DefaultSchema({
   html: { type: String, default: defaultHTML },
   css: { type: String, default: defaultCSS },
-  urls: { type: String, default: defaultURLS },
+  urls: { type: [String], default: defaultURLS },
 
-  enabled: { type: Boolean, default: false },
+  enabled: { type: Boolean, default: defaultEnabled },
 });
 
 module.exports = mongoose.model('Prank', schema, 'pranks');
